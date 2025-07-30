a6d59def8a502b8b525b3077df076fe2cb96f6e8
AndyDough <andynguyendo@gmail.com>
2025-07-29 20:57:46 -0700

    i'm batman

---
+++ keyboards/handwired/breadboard/rules.mk
@@ -0,0 +1,10 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+CONSOLE_ENABLE = yes
+VIA_ENABLE = yes
